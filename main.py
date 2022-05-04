45aa3ffc84e71a55610120fd689f758068fbd999
hp <ihabsalma12@yahoo.com>
2022-05-04 21:55:51 +0200

    committing files

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import math
 from sys import exit
 from pygame.locals import *
 
-#TODO graph class
+#TODO Graph class
 class Graph:
     def __init__(self):
         self.adj_matrix = []
         self.node_to_be_moved = Rect()
-
 
 class Button:
     def __init__(self, screen, colour, x, y, width, height, text = ''):
@@ -37,18 +36,36 @@ class Button:
                 return True
         return False
 
-
-#TODO finish arrow
-class Arrow: #TODO Rect obj!!!
-    def __init__(self, start_node, end_node, directed): #edge between two circles, and bool directed or not
+#TODO Arrow class (with weights and direction)
+class Edge: #TODO Rect obj!!!
+    def __init__(self, start_node, end_node, weight): #edge between two circles, and bool directed or not
+        self.weight = weight
         self.start_node = start_node
         self.end_node = end_node
-        self.directed = directed
+        self.start_rect = start_node.get_rect()
+        self.end_rect = end_node.get_rect()
+        self.draw_edge()
 
-    #def draw_arrow(self):
-        #pygame.draw.polygon(screen, (0, 0, 0),
-        #                    ((0, 100), (0, 200), (200, 200), (200, 300), (300, 150), (200, 0), (200, 100)))
+    def draw_edge(self):
+        if directed:
+            self.draw_directed()
+        else: self.draw_undirected()
 
+    def draw_directed(self):
+        arrow = pygame.transform.scale(pygame.image.load('arrow.png'),
+                               (abs(self.start_rect.center[0] - self.end_rect.center[0]),
+                                abs(self.start_rect.center[1] - self.end_rect.center[1])))
+        screen.blit(arrow, (20,20))
+        if self.weight == '':
+            font = pygame.font.SysFont("Calibri", 20)
+            text_surface = font.render('1', True, "black")
+            screen.blit(text_surface, (90,90))
+
+    #arrow = pygame.draw.polygon(screen, (0, 0, 0),
+        #((0, 100), (0, 200), (200, 200), (200, 300), (300, 150), (200, 0), (200, 100)))
+
+    def draw_undirected(self):
+        pygame.draw.line(screen, "dark grey", self.start_rect.center, self.end_rect.center, width = 10)
 
 #TODO finish node
 class Node:
@@ -79,38 +96,27 @@ pygame.init()
 clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((1000,500))
-screen.fill("grey")
+screen.fill("white")
 pygame.display.set_caption("CSE472 AI PROJECT")
-
-graph = pygame.Surface((500,480))
-graph.fill("white")
 
 nodes = []
 edges = [[]]
+adding_nodes_state = False
+drawing_edge_state = True
+directed = False
 
-
-
-#clicking = False
 
 while True:
-    add_node_btn = Button(screen, "light grey", 600, 50, 100, 30, text='Add node')
+    add_node_btn = Button(screen, "light grey", 750, 10, 100, 30, text='Add node')
     add_node_btn.draw_button("dark grey")
 
-    directed_btn = Button(screen, "light grey", 750, 50, 150, 30, text='Toggle directed')
+    directed_btn = Button(screen, "light grey", 750, 90, 150, 30, text='Toggle directed')
     directed_btn.draw_button("dark grey")
 
+    undirected_btn = Button(screen, "light grey", 750, 140, 170, 30, text='Toggle undirected')
+    undirected_btn.draw_button("dark grey")
+
     mouse_pos = pygame.mouse.get_pos()
-
-    #current_node_surf = pygame.Surface((20,20))
-    #current_node_surf.fill("white")
-    #current_node = Node(surf=current_node_surf, color="black", center_x=200, center_y=200, radius=20)
-    #current_node_rect = current_node_surf.get_rect()
-
-    #rot = 0
-    #loc = [mouse_pos[0], mouse_pos[1]]
-    #if clicking:
-        #rot -= 90
-    #screen.blit(pygame.transform.rotate(current_node.surface, rot), (loc[0], loc[1]))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -121,34 +127,32 @@ while True:
             if event.button == 1:
                 if add_node_btn.isOver(mouse_pos):
                     print("mouse button is down on Add Node btn")
-                    background = screen
-                    screen.blit(background, (0, 0))
-                    current_node_surf = pygame.transform.scale(pygame.image.load('R.png'), (40,40))
-                    current_node_rect = current_node_surf.get_rect()
-                    #current_node_surf = pygame.Surface((40,40))
-                    #current_node_surf.fill("yellow")
-                    #current_node_rect = pygame.draw.circle(surface=current_node_surf, color="green", center=(200,200), radius=20, width=3)
-                    graph.blit(current_node_surf, (200,200), current_node_rect)
-                    pygame.display.update()
-                    #current_node = Node(screen=graph, color="black", center_x=200, center_y=200, radius=20)
-                    #nodes.append(current_node)
-                if mouse_pos[0] > 200 and mouse_pos[1] > 200:
-                    for x in range(100):
-                        screen.blit(background, current_node_rect)  # erase
-                        position = (pygame.mouse.get_pos()[0] - 20, pygame.mouse.get_pos()[1] - 20) # move player
-                        graph.blit(current_node_surf, position, current_node_rect)  # draw new player
-                        pygame.display.update()  # and show it all
-                        #pygame.time.delay(100)  # stop the program for 1/10 second
+                    nodes.append(pygame.transform.scale(pygame.image.load('R.png'), (40,40)))
+                    adding_nodes_state = not adding_nodes_state
+
+                if directed_btn.isOver(mouse_pos):
+                    print("mouse over toggle directed")
+                    directed = True
+                    drawing_edge_state = True
+
+                if undirected_btn.isOver(mouse_pos):
+                    print("mouse over toggle undirected")
+                    directed = False
+                    drawing_edge_state = True
+
+                if mouse_pos[0] < 700 and mouse_pos[1] < 500 and len(nodes) and adding_nodes_state:
+                    screen.blit(nodes[-1], (mouse_pos[0]-20, mouse_pos[1]-20))
                     print("mouse button down on current node")
-                    #clicking = True
+                    print(len(nodes))
+                    adding_nodes_state = False
+
+                if len(nodes) >= 2 and drawing_edge_state:
+                    edge = Edge(nodes[0], nodes[1], 5)
 
         if event.type == MOUSEBUTTONUP:
             if event.button == 1:
-                #clicking = False
                 print("mouse button is up")
 
-    #graph.blit(current_node_surf, (200,200))
-    screen.blit(graph, (10,10))
     pygame.display.update()
     clock.tick(60)
 
