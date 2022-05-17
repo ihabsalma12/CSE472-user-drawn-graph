0a4b8ca4568574fa443da927fe9bc4e5ef740163
hp <ihabsalma12@yahoo.com>
2022-05-17 18:21:32 +0200

    heuristic buttons

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
 from pprint import pprint
 from tkinter import *
-
+import tkinter
+from tkinter import messagebox
 
 nodes = []
 edges = []
@@ -10,7 +11,6 @@ class Node:
     def __init__(self, canvas):
         self.canvas = canvas
         self.index = len(nodes)
-        self.visited = False
         #TODO look this up: https://tkinter-docs.readthedocs.io/en/latest/widgets/canvas.html?highlight=create_circle
         self.center_x = -1
         self.center_y = -1
@@ -18,6 +18,7 @@ class Node:
         self.bottom = -1
         self.left = -1
         self.right = -1
+        self.heuristic = -1 #flag
 
     def createNode(self, event):
         self.index = len(nodes)
@@ -34,6 +35,7 @@ class Node:
                                 self.center_y + 20,
                                 fill="grey", width=3, outline="black")
         self.canvas.create_text(self.center_x, self.center_y, font=("Arial", 12), text=str(self.index))
+
 
 class Edge:
     def __init__(self, canvas, is_directed):
@@ -101,7 +103,7 @@ class App:
         self.start_node = -1
         self.goal_nodes = set()
 
-        self.canvas = Canvas(root, height=480, width=730, bg="white")
+        self.canvas = Canvas(root, height=700, width=1000, bg="white")
         self.canvas.grid(sticky=NW, padx=10, pady=10)
         self.side_frame = Frame(root)
         self.side_frame.grid(row=0, column=1, sticky=NW)
@@ -118,22 +120,8 @@ class App:
         self.add_undirected_btn.bind("<Button-1>", self.startAddUndirected)
         self.add_undirected_btn.grid(row=2, column=0, sticky=NW)
 
-        self.weight_frame = Frame(self.side_frame, pady=20)
-        self.weight_frame.grid(row=1,column=0,sticky=NW)
-        self.weight_instr = Label(self.weight_frame, text="Enter below two nodes, and the weight between them.", pady=10)
-        self.weight_instr.grid()
-        self.weight_from = Entry(self.weight_frame, borderwidth=3, width=5)
-        self.weight_to = Entry(self.weight_frame, borderwidth=3, width=5)
-        self.weight_val = Entry(self.weight_frame, borderwidth=3, width=10)
-        self.weight_from.grid(sticky=NW)
-        self.weight_to.grid(sticky=NW)
-        self.weight_val.grid(sticky=NW)
-        self.add_weight_btn = Button(self.weight_frame, text="Add weight")
-        self.add_weight_btn.bind("<Button-1>", self.addWeight)
-        self.add_weight_btn.grid(row=4, column=0, sticky=NW)
-
-        self.btns_frame2 = Frame(self.side_frame, pady=20)
-        self.btns_frame2.grid(row=2, column=0)
+        self.btns_frame2 = Frame(self.side_frame, pady=10)
+        self.btns_frame2.grid(row=0, column=1, sticky = NW)
         self.start_node_btn = Button(self.btns_frame2, text="Select start node")
         self.goal_nodes_btn = Button(self.btns_frame2, text="Select goal nodes")
         self.start_node_btn.bind("<Button-1>", self.setStart)
@@ -141,16 +129,74 @@ class App:
         self.start_node_btn.grid(row=0, column=0)
         self.goal_nodes_btn.grid(row=0, column=1)
 
+        self.weight_frame = Frame(self.side_frame, pady=50)
+        self.weight_frame.grid(row=1,column=0,sticky=NW)
+        self.weight_instr1 = Label(self.weight_frame, text="Weight From:", pady=5)
+        self.weight_instr1.grid(row=0, column=0, sticky=NE)
+        self.weight_instr2 = Label(self.weight_frame, text="To:", pady=5)
+        self.weight_instr2.grid(row=1, column=0, sticky=NE)
+        self.weight_instr3 = Label(self.weight_frame, text="Value:", pady=5)
+        self.weight_instr3.grid(row=2, column=0, sticky=NE)
+        self.weight_from = Entry(self.weight_frame, borderwidth=3, width=5)
+        self.weight_to = Entry(self.weight_frame, borderwidth=3, width=5)
+        self.weight_val = Entry(self.weight_frame, borderwidth=3, width=10)
+        self.weight_from.grid(row=0, column =1, sticky=NW)
+        self.weight_to.grid(row = 1, column =1,sticky=NW)
+        self.weight_val.grid(row=2,column=1, sticky=NW)
+        self.add_weight_btn = Button(self.weight_frame, text="Add weight")
+        self.add_weight_btn.bind("<Button-1>", self.addWeight)
+        self.add_weight_btn.grid(row=4, column=1, sticky=NW)
+
+        self.heur_frame = Frame(self.side_frame, pady=50)
+        self.heur_frame.grid(row=1, column=1, sticky=NE)
+        self.heur_instr = Label(self.heur_frame, text="Node:", pady=5)
+        self.heur_instr.grid(row=0, column=0, sticky=NE)
+        self.heur_node = Entry(self.heur_frame, borderwidth=3, width=5)
+        self.heur_node.grid(row=0, column=1, sticky=NW)
+        self.heur_instr = Label(self.heur_frame, text="Heuristic:", pady=5)
+        self.heur_instr.grid(row=1, column=0, sticky=NE)
+        self.heur_val = Entry(self.heur_frame, borderwidth=3, width=5)
+        self.heur_val.grid(row=1, column=1, sticky=NW)
+        self.add_heur_btn = Button(self.heur_frame, text="Add heuristic")
+        self.add_heur_btn.bind("<Button-1>", self.changeHeuristic)
+        self.add_heur_btn.grid(row=2, column=1, sticky=NE)
+
         self.checkbox_frame = Frame(self.side_frame, pady=10)
         self.checkbox_frame.grid(row=3, column=0)
-        c1 = Checkbutton(self.checkbox_frame, text="BFS").grid(row=0, column=0, sticky=NW)
-        c2 = Checkbutton(self.checkbox_frame, text="DFS").grid(row=1, column=0, sticky=NW)
-        c3 = Checkbutton(self.checkbox_frame, text="Uniform Cost").grid(row=2, column=0, sticky=NW)
-        c4 = Checkbutton(self.checkbox_frame, text="Iterative Deepening").grid(row=2, column=0, sticky=NW)
-        c5 = Checkbutton(self.checkbox_frame, text="Greedy").grid(row=0, column=1, sticky=NW)
-        c6 = Checkbutton(self.checkbox_frame, text="A*").grid(row=1, column=1, sticky=NW)
-        l4 = Entry(self.checkbox_frame, borderwidth=3, width=5).grid(row=2, column=1, sticky=NW)
+        self.c1var = IntVar()
+        self.c2var = IntVar()
+        self.c3var = IntVar()
+        self.c4var = IntVar()
+        self.c5var = IntVar()
+        self.c6var = IntVar()
+        c1 = Checkbutton(self.checkbox_frame, text="BFS", onvalue = 1, offvalue = 0, variable=self.c1var).grid(row=0, column=0, sticky=NW)
+        c2 = Checkbutton(self.checkbox_frame, text="DFS", onvalue = 1, offvalue = 0, variable=self.c2var).grid(row=1, column=0, sticky=NW)
+        c3 = Checkbutton(self.checkbox_frame, text="Uniform Cost", onvalue = 1, offvalue = 0, variable=self.c3var).grid(row=2, column=0, sticky=NW)
+        c4 = Checkbutton(self.checkbox_frame, text="Iterative Deepening", onvalue = 1, offvalue = 0, variable=self.c4var).grid(row=2, column=0, sticky=NW)
+        c5 = Checkbutton(self.checkbox_frame, text="Greedy", onvalue = 1, offvalue = 0, variable=self.c5var).grid(row=0, column=1, sticky=NW)
+        c6 = Checkbutton(self.checkbox_frame, text="A*", onvalue = 1, offvalue = 0, variable=self.c6var).grid(row=1, column=1, sticky=NW)
+        self.search_btn = Button(self.checkbox_frame, text="Search")
+        self.search_btn.grid(pady=10, sticky=NW)
+        self.search_btn.bind("<Button-1>", self.checkHeuristic)
         #TODO finish checkboxes and link up with backend
+
+    def changeHeuristic(self, event):
+        nodes[int(self.heur_node.get())].heuristic = int(self.heur_val.get())
+        for node in nodes:
+            print("node #", node.index, " heuristic = ", node.heuristic)
+
+    def checkHeuristic(self, event):
+        if (self.c5var.get() == 1 or self.c6var.get () == 1) and len(nodes):
+            for node in nodes:
+                if node.heuristic == -1:
+                    messagebox.showerror("Incomplete Heuristics","Cannot perform informed search")
+                    self.search_btn.lift()
+                    break
+        self.startSearchWindow(event)
+
+    def startSearchWindow(self, event):
+        pass
+        #TODO
 
     def setStart(self, event):
         print("start")
@@ -281,6 +327,7 @@ class App:
 
 
 root = Tk()
-root.geometry("1100x500")
+root.geometry("1500x750")
+#root.attributes('-fullscreen', True)
 app = App()
 root.mainloop()
